495cec4b1d7a19e5de9879c8f1c99f51a4f9c5d0
Noah Syrkis <noah@syrkis.com>
2023-11-13 20:55:14 -0300

    fix jax model saving

--- syrkis/training/utils.py
+++ syrkis/training/utils.py
@@ -6,22 +6,33 @@
 # imports
 import os
 import numpy as np
+import pickle
 from jax.tree_util import tree_flatten, tree_unflatten
 
 
 def save_model_weights(params, path="model"):
     """Save JAX model weights to csvs."""
     flat_params, structure = tree_flatten(params)
-    np.savetxt(os.path.join(path, "structure.csv"), structure, delimiter=",")
     for i, param in enumerate(flat_params):
         np.savetxt(os.path.join(path, f"param_{i}.csv"), param, delimiter=",")
-    return
+    with open(os.path.join(path, "structure.pkl"), "wb") as f:
+        pickle.dump(structure, f)
 
-def load_model_weights(path):
-    """params dict from csvs."""
-    structure = np.loadtxt(path + "structure.csv", delimiter=",")
+
+def load_model_weights(path="model"):
+    """Load JAX model weights from csvs."""
+    # Load the structure
+    with open(os.path.join(path, "structure.pkl"), "rb") as f:
+        structure = pickle.load(f)
+
+    # Reconstruct flat_params from saved files
     flat_params = []
-    for i in range(len(structure)):
-        flat_params.append(np.loadtxt(path + f"param_{i}.csv", delimiter=","))
+    i = 0
+    while os.path.exists(os.path.join(path, f"param_{i}.csv")):
+        param = np.loadtxt(os.path.join(path, f"param_{i}.csv"), delimiter=",")
+        flat_params.append(param)
+        i += 1
+
+    # Reconstruct the original params structure
     params = tree_unflatten(structure, flat_params)
     return params
