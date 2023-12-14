db63009038777fc0940691ea44fbb4e6dbf4ff1c
Noah Syrkis <noah@syrkis.com>
2023-12-14 18:33:44 -0300

    syrkis package

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
-# setup.py at the root of your package directory
-
 from setuptools import setup, find_packages
+
+# Read the contents of your requirements.txt file
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 
 setup(
     name='syrkis',
     version='0.1.0',
     packages=find_packages(),
+    install_requires=requirements,  # Add this line
 )
