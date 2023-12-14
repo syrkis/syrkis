9e1f5c7fbe8fc14e9cba612a29cae42468dd575f
Noah Syrkis <noah@syrkis.com>
2023-12-14 19:03:57 -0300

    syrkis package

--- setup.py
+++ setup.py
@@ -1,14 +1,7 @@
 from setuptools import setup, find_packages
 
-# Read the contents of your requirements.txt file
-with open('requirements.txt') as f:
-    requirements = [line.strip() for line in f 
-                    if line.strip() and not line.startswith('#') 
-                    and not line.startswith('-e')]
-
 setup(
     name='syrkis',
     version='0.1.0',
     packages=find_packages(),
-    install_requires=requirements,
 )
